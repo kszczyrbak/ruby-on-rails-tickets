98bfaf1f94ec4427b1bf2119c6d945c61f32931a
Chris <kszczyrbak8@gmail.com>
2020-01-19 19:55:01 +0100

    bootstrap redesign

--- Gemfile
+++ Gemfile
@@ -18,9 +18,13 @@ gem "turbolinks", "~> 5"
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem "jbuilder", "~> 2.7"
 
-gem "bootstrap-sass", "~> 3.4", ">= 3.4.1"
+gem "bootstrap", "~> 4.0.0"
+
+gem "jquery-rails"
 
 gem "devise", "~> 4.7", ">= 4.7.1"
+
+gem "bootstrap_form", "~> 4.0"
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("bootstrap-sprockets")
-require("bootstrap")
+
+import 'bootstrap'
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
